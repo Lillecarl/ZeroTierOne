46e9e324fbf922b859e5684d2118db77c24a61de
Carl Hjerpe <carl.hjerpe@dialect.se>
2018-01-16 23:50:21 +0100

    Simplify JoinNetworkView

--- windows/WinUI/JoinNetworkView.xaml.cs
+++ windows/WinUI/JoinNetworkView.xaml.cs
@@ -20,107 +20,28 @@ namespace WinUI
     /// </summary>
     public partial class JoinNetworkView : Window
     {
-        Regex charRegex = new Regex("[0-9a-fxA-FX]");
         Regex wholeStringRegex = new Regex("^[0-9a-fxA-FX]+$");
 
         public JoinNetworkView()
         {
             InitializeComponent();
-
-            DataObject.AddPastingHandler(joinNetworkBox, onPaste);
-            DataObject.AddCopyingHandler(joinNetworkBox, onCopyCut);
         }
 
-        private void joinNetworkBox_OnTextEntered(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !charRegex.IsMatch(e.Text);
-
-            if ( (joinNetworkBox.Text.Length + e.Text.Length) == 16)
-            {
-                joinButton.IsEnabled = true;
-            }
-            else
-            {
-                joinButton.IsEnabled = false;
-            }
-        }
-
-        private void joinNetworkBox_OnKeyDown(object sender, KeyEventArgs e)
-        {
-            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
-            {
-                if (e.Key == Key.X && joinNetworkBox.IsSelectionActive)
-                {
-                    // handle ctrl-x removing characters
-                    joinButton.IsEnabled = false;
-                }
-            }
-            else if (e.Key == Key.Delete || e.Key == Key.Back)
-            {
-                if ((joinNetworkBox.Text.Length - 1) == 16)
-                {
-                    joinButton.IsEnabled = true;
-                }
-                else
-                {
-                    joinButton.IsEnabled = false;
-                }
-            }
-            else
-            {
-                if ((joinNetworkBox.Text.Length + 1) > 16)
-                {
-                    e.Handled = true;
-                }
-            }
-        }
-
-        private void onPaste(object sender, DataObjectPastingEventArgs e)
-        {
-            var isText = e.SourceDataObject.GetDataPresent(DataFormats.UnicodeText, true);
-            if (!isText)
-            {
-                joinButton.IsEnabled = false;
-                return;
-            }
-
-            var text = e.SourceDataObject.GetData(DataFormats.UnicodeText) as string;
-
-            if (!wholeStringRegex.IsMatch(text))
-            {
-                e.Handled = true;
-                e.CancelCommand();
-            }
-
-            if (text.Length == 16 || (joinNetworkBox.Text.Length + text.Length) == 16)
-            {
-                joinButton.IsEnabled = true;
-            }
-            else if (text.Length > 16 || (joinNetworkBox.Text.Length + text.Length) > 16)
-            {
-                e.Handled = true;
-                e.CancelCommand();
-            }
-            else
-            {
-                joinButton.IsEnabled = false;
-            }
-        }
-
-        private void onCopyCut(object sender, DataObjectCopyingEventArgs e)
-        {
-            
-        }
+        public string NetworkID { get; set; } = "";
+        public bool AllowDefault { get; set; } = false;
+        public bool AllowGlobal { get; set; } = false;
+        public bool AllowManaged { get; set; } = true;
 
         private void joinButton_Click(object sender, RoutedEventArgs e)
         {
-            bool allowDefault = allowDefaultCheckbox.IsChecked.Value;
-            bool allowGlobal = allowGlobalCheckbox.IsChecked.Value;
-            bool allowManaged = allowManagedCheckbox.IsChecked.Value;
-
-            APIHandler.Instance.JoinNetwork(this.Dispatcher, joinNetworkBox.Text, allowManaged, allowGlobal, allowDefault);
+            APIHandler.Instance.JoinNetwork(this.Dispatcher, NetworkID, AllowManaged, AllowGlobal, AllowDefault);
 
             Close();
+        }
+
+        private void joinNetworkBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            joinButton.IsEnabled = joinNetworkBox.Text.Length == 16 && wholeStringRegex.IsMatch(joinNetworkBox.Text);
         }
     }
 }
